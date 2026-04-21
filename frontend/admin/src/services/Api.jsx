77b96a0b3b01f96cbdad683f8c16e024b0cb43c6
WebCasting28 <ansarisikkim786@gmail.com>
2026-04-21 17:56:52 +0530

    fixt: chnage in base Api

--- frontend/admin/src/services/Api.jsx
+++ frontend/admin/src/services/Api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const baseApi = axios.create({
-    baseURL: "http://localhost:8080/api/admin",
+    // baseURL: "http://localhost:8080/api/admin",
+    baseURL: "https://fixnest-6t18.onrender.com/api/admin",
     withCredentials: true
 })
 
--- frontend/user/src/services/Api.jsx
+++ frontend/user/src/services/Api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const baseApi = axios.create({
-    baseURL:"http://localhost:8080/api/v1",
+    // baseURL:"http://localhost:8080/api/v1",
+    baseURL:"https://fixnest-6t18.onrender.com/api/v1",
     withCredentials:true
 })
 
