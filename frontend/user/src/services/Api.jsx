634274d1e7c0f3302071974f48e9d8d646cc593f
WebCasting28 <ansarisikkim786@gmail.com>
2026-04-22 13:25:30 +0530

    fix: update baseURL configuration in Api service

--- frontend/user/src/services/Api.jsx
+++ frontend/user/src/services/Api.jsx
@@ -1,9 +1,8 @@
 import axios from "axios"
 
 const baseApi = axios.create({
-    // baseURL:"http://localhost:8080/api/v1",
-    // baseURL: import.meta.env.VITE_API_URL,
-     baseURL: import.meta.env.VITE_TRIAL_URI,
+     baseURL: import.meta.env.VITE_API_URL,
+    //  baseURL: import.meta.env.VITE_TRIAL_URI,
     withCredentials:true
 })
 
